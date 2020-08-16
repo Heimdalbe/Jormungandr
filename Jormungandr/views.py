9d4d046d3ee2902cb6770358b0a885ec1e555dba
Nick Lersberghe <nick.lersberghe@student.hogent.be>
2020-08-16 21:36:52 +0200

    Added select year option to view different praesidia years

--- Jormungandr/views.py
+++ Jormungandr/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 
-from Backend.models import CMS, PraesidiumMember
+from Backend.models import CMS, PraesidiumMember, PraesidiumYear
 
 
 def index(request):
-
     return render(request, 'Jormungandr/index.html', {})
 
 
@@ -14,9 +13,27 @@ def cms(request, page):
 
 
 def praesidium(request):
-    # Add if else for checking for GET and POST
-    # Way to select which praesidiumyear to view
-    praesidium = PraesidiumMember.objects.all()
-    return render(request, 'Jormungandr/praesidium.html', {'praesidium': praesidium})
+    years = sorted(PraesidiumYear.objects.all(), key=lambda s: s.start, reverse=True)
+    year = years[0]  # Default year
+    if request.method == "GET":
+        chosenyear = request.GET.get("year")
+        if chosenyear and chosenyear != "0":
+            year = PraesidiumYear.objects.filter(id=chosenyear).get()
+    members = PraesidiumMember.objects.filter(year=year)
 
+    return render(request, 'Jormungandr/praesidium.html',
+                  {'praesidium': members, "years": years, "selectedyear": year.id})
 
+# if request.method == 'GET':         user = request.GET.get("user")
+# month = request.GET.get("month")
+# if user:             if
+# month: queryset_list = Shift.objects.filter(user_id=user, start_timemonth=month.split("|")[0],
+#                                             start_timeyear=month.split("|")[1]).order_by('-start_time')
+# direct += "?user=" + request.GET.get("user")
+# direct += "%26month=" + request.GET.get("month")
+# context['url_user'] = int(user)
+# context['url_month'] = month             else:                 queryset_list = Shift.objects.filter(
+#     user_id=user).order_by('-start_time')
+# direct += "?user=" + request.GET.get("user")
+# context['url_user'] = int(user) elif month: queryset_list = Shift.objects.filter(start_timemonth=month.split("|")[0],
+#                                                                                  start_timeyear=month.split("|")
