fa1260b33027b14697a719cd8a50553943f36f0b
Nick <nicklersberghe@gmail.com>
2021-04-24 19:53:00 +0200

    Fixed name on edge in graph

--- Jormungandr/util/tools.py
+++ Jormungandr/util/tools.py
@@ -1,19 +1,24 @@
-def graph_nodes_to_json(nodes):
+from Backend.models import GraphNode
+from typing import List
+
+
+def graph_nodes_to_json(nodes: List[GraphNode]):
     result = {"nodes": [], "edges": []}
-    index = 0
+
     for node in nodes:
         result["nodes"].append({
             "id": node.name,
             "height": 50,
             "display": node.display_name(),
-            "x": index * 50,
-            "y": 0,
+
             "fill": {
                 "src": node.photo
             },
         })
-        result["edges"].append({"from": node.parent.display_name(), "to": node.display_name()}) if node.parent else ()
-
-        index += 1
+        result["edges"].append(
+            {"from": node.parent.name,
+             "to": node.name,
+             "from_display": node.parent.display_name(),
+             "to_display": node.display_name()}) if node.parent else ()
 
     return result
