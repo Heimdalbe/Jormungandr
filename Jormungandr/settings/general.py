58f6e3b06d21a6fb244111b97a2b8eb73d431227
De Bock Benny <bdebock@protonmail.com>
2023-02-11 02:40:28 +0100

    Add settings for s3 media upload

--- Jormungandr/settings/general.py
+++ Jormungandr/settings/general.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'markdownx',
     'multiselectfield',
+    'storages',
     'Jormungandr',
     'Backend',
     'Intranet'
@@ -140,8 +141,24 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'data/static')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'data/files')
-MEDIA_URL = '/files/'
+
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
+
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'data/files')
+    MEDIA_URL = '/files/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
